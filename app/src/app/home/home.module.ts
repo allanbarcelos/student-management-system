0fa92ac7eed4ee66a688a46c90ced5c9075a0570
Alena <alenabelovadmvn@gmail.com>
2025-03-28 22:28:43 -0400

    Declaration of component in HomeModule

--- app/src/app/home/home.module.ts
+++ app/src/app/home/home.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { CourseRegistrationComponent } from './course-registration/course-registration.component';
+
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent, 
+    CourseRegistrationComponent,
   ],
   imports: [
     CommonModule,
