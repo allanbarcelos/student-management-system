977d88ccdd17a0e608090184e64a30ab9265d7c8
Alena <alenabelovadmvn@gmail.com>
2025-03-28 23:51:34 -0400

    Component class and decorator added to course-registration.component.ts

---
+++ app/src/app/home/course-registration/course-registration.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-course-registration',
+  templateUrl: './course-registration.component.html',
+  styleUrls: ['./course-registration.component.scss']
+})
+export class CourseRegistrationComponent {}
