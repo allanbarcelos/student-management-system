9a3f18f4e536ae7b2488b372faf69af987f30d9b
Alena <alenabelovadmvn@gmail.com>
2025-03-28 22:33:12 -0400

    Route for Course Registration Component added

--- app/src/app/home/home-routing.module.ts
+++ app/src/app/home/home-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home.component';
+import { CourseRegistrationComponent } from './course-registration/course-registration.component';
 
-const routes: Routes = [{ path: '', component: HomeComponent }];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'course-registration', component: CourseRegistrationComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
