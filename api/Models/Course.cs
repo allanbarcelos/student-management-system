ac87ad1fe4dbe47b135a83019d691b9ce5840aaf
Tara niaki <taraniaki@gmail.com>
2025-04-09 21:06:13 -0400

    created course migration

---
+++ api/Models/Course.cs
@@ -0,0 +1,23 @@
+using System;
+ 
+namespace api.Models
+
+{
+
+    public class Course
+
+    {
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Code { get; set; }
+
+        public int Credits { get; set; }
+
+    }
+
+}
+
+ 
