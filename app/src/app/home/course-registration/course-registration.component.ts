95f09ce26ee4f5097944a0a5e029adc556a1e426
Alena <alenabelovadmvn@gmail.com>
2025-04-18 16:17:02 -0400

    html of the form adjusted to work with ngModel. + course registration component class completed.

--- app/src/app/home/course-registration/course-registration.component.ts
+++ app/src/app/home/course-registration/course-registration.component.ts
@@ -1,8 +1,35 @@
 import { Component } from '@angular/core';
+import { CourseListingService, Course } from '../../course-listing/course-listing.service';
 
 @Component({
   selector: 'app-course-registration',
   templateUrl: './course-registration.component.html',
   styleUrls: ['./course-registration.component.scss']
 })
-export class CourseRegistrationComponent {}
+export class CourseRegistrationComponent {
+  constructor(private courseService: CourseListingService) {}
+
+  registerCourse(formRef: any) {
+    const form = formRef.value;
+  
+    const courseData: Course = {
+      id: 0, // or auto-gen
+      name: form.courseName,
+      description: form.description,
+      startDate: form.startDate || '',
+      endDate: form.endDate || '',
+      instructor: form.instructor || '',
+    };
+
+    this.courseService.createCourse(courseData).subscribe({
+      next: (res) => {
+        alert('Course registered successfully!');
+        formRef.resetForm(); 
+      },
+      error: (err) => {
+        console.error(err);
+        alert('Error registering course.');
+      }
+    });
+  }
+}
